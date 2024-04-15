1ac2cb8a8de71ecb7003c23704a5e2e43c75fde9
Vastayaa404 <imacproffesional@gmail.com>
2024-04-15 16:51:43 +0300

    App

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='wrapper'>
+      <div className='background'><h1>Hi. It seems you were looking for something? It's definitely not here :)</h1><p>404</p></div>
+    </div>
+  );
+}
