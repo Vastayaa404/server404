ac947f931867f9d0cf7d15f2557e2af63a357682
Vastayaa404 <imacproffesional@gmail.com>
2024-04-15 21:32:48 +0300

    bug fixes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className='wrapper'>
-      <div className='background'><h1>Hi. It seems you were looking for something? It's definitely not here :)</h1><p>404</p></div>
+      <div className='background'>
+        <div className='background_text'>
+          <h1>Hi. It seems you were looking for something? It's definitely not here :)</h1><p>404</p>
+        </div>
+        <img className='background_image' src={'q.jpg'} alt='backgroundImage' />
+      </div>
     </div>
   );
 }
